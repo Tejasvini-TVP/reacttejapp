64323b4ec1fd0382ea4057062a6283af7806861a
Tejasvini <tejasvini.patel1311@gmail.com>
2024-01-08 18:20:54 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Resturant from './component/Basics/Resturant';
-import UseState from './component/Hooks/use.State';
+// import Resturant from './component/Basics/Resturant';
+// import UseState from './component/Hooks/use.State';
+import UseEffect from './component/Hooks/useEffect';
 const App = () => {
    // return <Resturant/>;
-   return <UseState/>;
+   return <UseEffect />;
 };
 
  export default App;
--- src/component/Hooks/useEffect.js
+++ src/component/Hooks/useEffect.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import "./style.css";
 
 
-const UseState = () => {
-    const initialData = 10;
+const useEffect = () => {
+
+
     const [myNum, setMyNum] = React.useState(0)
    
     return(
@@ -17,18 +18,9 @@ const UseState = () => {
              <span></span> 
         INCR  
             </div>
-            
-            <div class="button2"  onClick={() => setMyNum(myNum - 1)}>
-              {/* onClick={() => (myNum > 0 ? setMyNum(myNum -1): setMyNum(0))}   ) 0 a stop */}
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                DECR
-            </div>
             </div>
          
         </>
     )
 }
-export default UseState;
+export default useEffect;
