06e81660d46a0d82076d459faa10a652d72eeefb
Tejasvini <tejasvini.patel1311@gmail.com>
2024-01-08 22:26:59 +0530

    UseRudcer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 // import Resturant from './component/Basics/Resturant';
 //  import UseState from './component/Hooks/use.State';
 import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
 const App = () => {
    // return <Resturant/>;
-   return <UseEffect />;
+   return <UseReducer/>;
 };
 
  export default App;
---
+++ src/component/Hooks/useReducer.js
@@ -0,0 +1,40 @@
+import React, { useReducer, useState } from "react";
+import "./style.css";
+
+const reducer = (state,action) => {};
+const UseReducer = () => {
+    const UseReducer = () =>{
+
+    }
+
+    const initialData = 10;
+    // const [myNum, setMyNum] = React.useState(0)
+   
+    const [state,dispatch] = useReducer(reducer,intialData);
+
+    return(
+        <>
+        <div className="center_div">
+            <p>{myNum}</p>
+            <div class="button2" onClick={() => setMyNum(myNum + 1)}>
+             <span></span> 
+             <span></span> 
+             <span></span> 
+             <span></span> 
+        INCR  
+            </div>
+            
+            <div class="button2"  onClick={() => setMyNum(myNum - 1)}>
+              {/* onClick={() => (myNum > 0 ? setMyNum(myNum -1): setMyNum(0))}   ) 0 a stop */}
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                DECR
+            </div>
+            </div>
+         
+        </>
+    )
+}
+export default UseReducer ;
