98a0219b4bd14d21e2951480f408a7d258ccee21
Tejasvini <tejasvini.patel1311@gmail.com>
2024-01-18 17:32:36 +0530

    Add Item

--- src/component/todoreact/todo.js
+++ src/component/todoreact/todo.js
@@ -6,8 +6,16 @@ const todo = () => {
       <div className="main-div">
       <div className="child-div">
         <figure>
-            <img src="./images/todo
+            <img src="./images/todo.svg" alt="todologo"/>
+            <figcaption>Add Your List Here✌️</figcaption>
         </figure>
+        <div className="addItem">
+            <input
+             type="text"
+             placeholder='✍️ Add Item'
+             className='form-control'
+             />
+        </div>
       </div>
       </div>
     </>
