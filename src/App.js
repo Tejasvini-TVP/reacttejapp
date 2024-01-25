45337b822b281a5112b64da77fde96b6d73344f9
Tejasvini <tejasvini.patel1311@gmail.com>
2024-01-25 17:50:24 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Temp from './component/weather/temp';
+import Todo from './component/todoreact/todo';
 
 // import Resturant from './component/Basics/Resturant';
 //  import UseState from './component/Hooks/use.State';
@@ -7,7 +8,7 @@ import UseEffect from './component/Hooks/useEffect';
 import UseReducer from './component/Hooks/useReducer';
 const App = () => {
    // return <Resturant/>;
-   return <Temp/>;
+   return <Todo/>;
 };
 
  export default App;
