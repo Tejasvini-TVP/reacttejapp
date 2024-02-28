54a4173231d867c11be02ecd1b8a9a8806898aa3
Tejasvini <tejasvini.patel1311@gmail.com>
2024-02-28 15:38:44 +0530

    v

--- vite/reactproject/src/App.jsx
+++ vite/reactproject/src/App.jsx
@@ -8,27 +8,7 @@ function App() {
 
   return (
     <>
-    <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
-    <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-  <img class="w-24 h-24 rounded-full mx-auto" src="https://images.pexels.com/photos/16937234/pexels-photo-16937234/free-photo-of-a-close-up-of-some-orange-and-white-tulips.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="384" height="512"/>
-  <div class="pt-6 text-center space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+    
     </>
   )
 }
