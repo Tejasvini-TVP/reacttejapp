6742eb14716128d6ee1bad8adbcd6a06861a6ce9
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-01 18:54:58 +0530

    password

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -9,11 +9,34 @@ function App() {
   const [password,setPassword] = useState("")
 
 
-  const passwordGenerator = useCallback(fn, [length,numberAllowed,charAllowed,setPassword])
+  const passwordGenerator = useCallback(() => {
+    let pass =""
+    let str ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-+_{}[]`~"
+    
+   for (let i = 1; i <= Array.length; i++){
+    let char = Math.floor(math.random() * str.length + 1)
+    pass = str.charAt(char)
+   }
+   setPassword(pass)
+  },
+ [length,numberAllowed,charAllowed,setPassword])
   return (
     <>
-      <h1 className='text-4xl text-center 
-      text-white'>Password generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 
+      text-orange-500'>
+        <h1 className='text-white text-center'>PasswordGenerator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+           <input 
+           type='text'
+           value={password}
+           className='outline-none w-full py-1 px-3'
+           placeholder='Password'
+           readOnly
+           />
+        </div>
+      </div>
     </>
   )
 }
