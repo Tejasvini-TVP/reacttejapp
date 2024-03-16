318f2b96fcaf76598800aebcca973bbee575a83f
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-16 17:08:13 +0530

    reactRouter

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header/Header.jsx'
-import Footer from './components/Footer/Footer.jsx'
-import Home from './components/Home/Home.jsx'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -4,20 +4,20 @@ import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
-import Home from './components/Home/Home.jsx'
-import About from './components/About/About.jsx'
-import Contact from './components/Contact/Contact.jsx'
-import User from './components/User/User.jsx'
-import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import User from './components/User/User'
+import Github,{githubInfoLoader} from './components/Github/Github'
+
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
-      <Route path='user/:userid' element={<User />} />
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>} />
+      <Route path='about' element={<About/>} />
+      <Route path='contact' element={<Contact/>} />
+      <Route path='user/:userid' element={<User/>} />
       <Route 
       loader={githubInfoLoader}
       path='github' 
