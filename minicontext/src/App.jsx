634c97b7233ea6a7a5681637274fe1c64f98a3fc
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-18 23:39:42 +0530

    Finaltouch

--- minicontext/src/App.jsx
+++ minicontext/src/App.jsx
@@ -4,13 +4,12 @@ import Profile from './components/Profile'
 import UserContextProvider from './context/UserContextProvider'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <UserContextProvider>
     <h1>react with Chai and share is important</h1>
     <Login  />
-    <profile />
+    <Profile />
     </UserContextProvider>
   )
 }
--- minicontext/src/context/UserContextProvider.jsx
+++ minicontext/src/context/UserContextProvider.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import UserContext from "./UserContext";
 
 const UserContextProvider = ({children}) => {
-    const [user,setUser] = React.useState(null)
+    const [user, setUser] = React.useState(null)
     return(
-        <UserContext.Provider value={{user,setUser}}>
-          {children}
+        <UserContext.Provider value={{user, setUser}}>
+        {children}
         </UserContext.Provider>
     )
-
 }
 
 export default UserContextProvider
