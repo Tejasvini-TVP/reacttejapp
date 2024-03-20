e207d4314dcbfa5451ddbe644a32059859ab5e00
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-20 23:03:23 +0530

    useTheme

--- themeswitcher/src/contexts/theme.js
+++ themeswitcher/src/contexts/theme.js
@@ -5,3 +5,9 @@ import {createContext,useContext} from "react";
         darkTheme: () => {},
         lightTheme: () => {},
     })
+
+    export const ThemeProvider = ThemeContext.Provider
+
+    export default function useTheme(){
+        return useContext(ThemeContext)
+    }
