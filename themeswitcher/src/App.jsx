65bc14d190749f45739dfadf7c7b58937aa21c5c
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-20 23:13:54 +0530

    hooks

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,9 +1,10 @@
+import { ThemeProvider } from "./contexts/theme"
 
 function App() {
-  
-  return (
-   
-   
+  const [themeMode,setThemeMode] = useState("light")
+
+return (
+<ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -15,7 +16,7 @@ function App() {
                     </div>
                 </div>
             </div>
-
+</ThemeProvider> 
     
   )
 }
