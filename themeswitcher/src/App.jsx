105255f539cc803a2d2f15c9e1e7572d7d0d30d1
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-20 23:52:45 +0530

    improt file

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,4 +1,8 @@
+import { useEffect,useState } from "react"
+import './App.css'
 import { ThemeProvider } from "./contexts/theme"
+import ThemeBtn from "./components/ThemeBtn"
+import Card from "./components/Card"
 
 function App() {
   const [themeMode,setThemeMode] = useState("light")
