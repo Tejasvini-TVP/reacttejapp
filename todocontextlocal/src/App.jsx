893662f6c9cbe3471d6d22fcd1af8bc1b4670685
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-21 22:51:48 +0530

    spread

--- todocontextlocal/src/App.jsx
+++ todocontextlocal/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState,useEffect } from 'react'
+import { useState,useEffect } from 'react'  
 import { TodoProvider } from './contexts'
 import './App.css'
 
 function App() {
  const [todos,setTodos] =useState([])
 
+ const addTodo =(todo) => {
+  // (...) spread
+  setTodos((prev) => [{id:Date.now(),...todo}, ...prev])
+ }
   return (
-    <TodoProvider>
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
   <div className="bg-[#172842] min-h-screen py-8">
             <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
