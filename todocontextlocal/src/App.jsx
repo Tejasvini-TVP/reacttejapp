f0e2146468878568282bee8bad16951cdd956677
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-22 00:13:04 +0530

    todo ids

--- todocontextlocal/src/App.jsx
+++ todocontextlocal/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
  }
 
  const toggleComplete = (id) => {
-  setTodos((prev) => prev.map((prevTodo) => prevTodo === id?
+  setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id?
   {...prevTodo,completed: !prevTodo.completed}
   :prevTodo))
  }
@@ -46,7 +46,8 @@ useEffect(() => {
             <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        {/* Todo form goes here */}
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
