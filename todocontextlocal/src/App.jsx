d22d891b0b4bca38e10995d6df19198f59f625f5
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-22 00:14:21 +0530

    final touch

--- todocontextlocal/src/App.jsx
+++ todocontextlocal/src/App.jsx
@@ -39,7 +39,6 @@ useEffect(() => {
   localStorage.setItem("todos", JSON.stringify(todos))
 }, [todos])
 
-
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
   <div className="bg-[#172842] min-h-screen py-8">
