0401a11257692dcfcdc3e196f31abcdb2c275d42
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-22 18:52:05 +0530

    todoSlice

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -7,5 +7,21 @@ const initialState ={
 export const todoSlice = createSlice({
     name: 'todo',
     initialState,
-    reducers: {}
+    reducers: {
+        addTodo: (state,action) => {
+            const todo = {
+               id: nanoid(),
+               Text: action.payload
+            }
+           state.todos.push(todo)
+        },
+        removeTodo: (state,action) => {
+          state.todos = state.todos.filter((todo)  => todo.
+          id !== action.payload)  
+        },
+    }
 })
+
+export const {addTodo,removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
