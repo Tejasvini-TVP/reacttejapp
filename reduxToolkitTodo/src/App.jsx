e93e4fea9ace85569160e2d28824ecde19a92218
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-22 22:38:35 +0530

    setup

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -2,20 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import AddTodo from './components/addTodo'
 import Todos from './components/Todos'
-import { Provider } from 'react-redux'
-import {store} from './app/store'
+
 
 function App() {
   
   return (
-    
-     <Provider store={store}>
-    <App />
-
+    <>
       <h1>Learn about redux toolkit</h1>
       <AddTodo />
       <Todos />
-    </Provider>
+    </>
   )
 }
 
