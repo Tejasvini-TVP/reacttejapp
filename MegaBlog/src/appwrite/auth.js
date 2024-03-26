ecc4c3bb71a3f31985001022be831ca4ea9868ee
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-26 20:04:19 +0530

    email&password

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -14,11 +14,26 @@ export class Authservice{
 
     async createAccount({email,password,name}) {
         try{
-
+           const userAccount = await this.account.create(ID.unique(),email,password,name);
+           if(userAccount) {
+           
+            return this.login({email,password})
+            // call anthor method
+           } else{
+            return userAccount;
+           }
         }catch (error) {
             throw error;
         }
     }
+    async login({email,password}) {
+      try{
+        return await this.account.createEmailSession(email,password);
+      } catch (error) {
+        throw error;
+      } 
+    }
+    
 }
 
 const authservice = new Authservice();
