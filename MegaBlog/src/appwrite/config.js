95772eb2f0349d5a45fa8f1328c31eefb791e9ad
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-27 16:39:34 +0530

    create post

--- MegaBlog/src/appwrite/config.js
+++ MegaBlog/src/appwrite/config.js
@@ -13,6 +13,26 @@ export  class service{
         this.databases = new this.databases(this.client)  
         this.bucket = new Storage(this.client)  
     }
+  // create post
+    async createPost({title,slug,content,status,userId}) {
+       try{
+        return await this.databases.createDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
+            {
+               title,
+               content,
+               featuredImage,
+               status,
+               userId,
+            }
+            
+        )
+       } catch(error) {
+        console.log("Appwrite service :: createPost :: error",error);
+       }
+    }
 }
 
 const service = new Service()
