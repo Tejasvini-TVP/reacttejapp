71e9e365b124cb0de3c5fc4ad9bf490f151920f2
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-28 19:54:12 +0530

    signup,login

--- MegaBlog/src/components/Header/index.js
+++ MegaBlog/src/components/Header/index.js
@@ -1,9 +1,8 @@
 import Header from "./Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container";
 import Logo from "../Logo";
 import LogoutBtn from "../LogoutBtn";
-
+import Container from "../container/Container";
 
 export {
     Header,
