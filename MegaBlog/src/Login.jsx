2c5d400d5be93250a829b9ebc0e7d10ba7a247d5
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-28 21:37:18 +0530

    navigate

--- MegaBlog/src/Login.jsx
+++ MegaBlog/src/Login.jsx
@@ -2,15 +2,32 @@ import React, {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import { login as authLogin } from '../store/authSlice'
 import {Button, Input, Logo} from "./index"
-import {useDispatch} from "react-redux"
+import { useDispatch} from "react-redux"
 import authService from "../appwrite/auth"
 import {useForm} from "react-hook-form"
-function Login() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
+
+function Login() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+    const [error, setError] = useState("")
+
+    const login = async(data) => {
+        setError("")
+        try{
+          const session = await authService.login(data)
+          if (session) {
+            const userData = await authService.
+            getCurrentUser()
+            if(userData) dispatch(authLogin(userData)) 
+          }
+        } catch (error) {
+            setError(error.message)
+       
+    }
+    
+}
+return
+}
 export default Login
