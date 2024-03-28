80137f569d9809ebbca2f032419937888c3971ba
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-28 21:15:10 +0530

    login

--- MegaBlog/src/components/PostCard.jsx
+++ MegaBlog/src/components/PostCard.jsx
@@ -8,9 +8,12 @@ function PostCard({$id, title, featuredImage}) {
      <Link to={`/post/${$id}`}>
         <div className='w-full bg-grey-100 rounder-xl p-4'>
             <div className='w-full justify-center mb-4'>
-            <img src={appwriteService.getFilePreview(featuredImage)} alt="" />
+            <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
+            className='rounded-xl' />
 
-            </div>
+         </div>
+         <h2
+         className='text-xl font-bold'>{title}</h2>
         </div>
         </Link>
   )
