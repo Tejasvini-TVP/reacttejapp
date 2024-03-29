59f57739e69236bf500eccf9bccbe93e42dc7eab
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-29 18:42:30 +0530

    postform complete

--- MegaBlog/src/post-form/PostForm.jsx
+++ MegaBlog/src/post-form/PostForm.jsx
@@ -56,13 +56,70 @@ function PostForm({post}) {
             .toLowerCase()
             .replace(/^[a-zA-Z\d\s]+/g, '-')
             .replace(/\s/g, '-')
-        return ''
-    },[])
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default PostForm
+        return ''
+
+    },[])
+
+  React.useEffect(() => {
+    const Subscription = watch((value,{name}) => {
+        if (name === 'title') {
+            setValue('slug',slugTransform(value.title,{shouldValidate}))
+        }
+    })
+
+    return () => {
+        Subscription.unSubscription()
+    }
+  }, [watch,slugTransform,setValue])
+
+  return (
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+    <div className="w-2/3 px-2">
+        <Input
+            label="Title :"
+            placeholder="Title"
+            className="mb-4"
+            {...register("title", { required: true })}
+        />
+        <Input
+            label="Slug :"
+            placeholder="Slug"
+            className="mb-4"
+            {...register("slug", { required: true })}
+            onInput={(e) => {
+                setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+            }}
+        />
+        <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
+    </div>
+    <div className="w-1/3 px-2">
+        <Input
+            label="Featured Image :"
+            type="file"
+            className="mb-4"
+            accept="image/png, image/jpg, image/jpeg, image/gif"
+            {...register("image", { required: !post })}
+        />
+        {post && (
+            <div className="w-full mb-4">
+                <img
+                    src={appwriteService.getFilePreview(post.featuredImage)}
+                    alt={post.title}
+                    className="rounded-lg"
+                />
+            </div>
+        )}
+        <Select
+            options={["active", "inactive"]}
+            label="Status"
+            className="mb-4"
+            {...register("status", { required: true })}
+        />
+        <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+            {post ? "Update" : "Submit"}
+        </Button>
+    </div>
+</form>
+);
+}
