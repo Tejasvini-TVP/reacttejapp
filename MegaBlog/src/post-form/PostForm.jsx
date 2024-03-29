04c731a815413a051b443c6dfa0a1664294ebcb0
Tejasvini <tejasvini.patel1311@gmail.com>
2024-03-29 18:27:21 +0530

    method

--- MegaBlog/src/post-form/PostForm.jsx
+++ MegaBlog/src/post-form/PostForm.jsx
@@ -5,7 +5,59 @@ import appwriteService from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-function PostForm() {
+function PostForm({post}) {
+    const {register,handleSubmit,watch,setValue,control,getvalues} = useForm ({
+        defaultValues: {
+            title: post?.title || '',
+            slug: post?.slug || '',
+            content: post?.content || '',
+            status: post?.status || 'active',
+            
+        }
+    })
+
+    const submit = async (data) => {
+        if(post) {
+            const file = data.image[0] ? appwriteService.
+            uploadFile(data.image[0]) : null
+
+            if (file) {
+                appwriteService.deleteFile(post.featuredImage)
+            }
+            const dbPost = await appwriteService.uploadPost(post.$id,{
+                ...data,
+                featuredImage: file? file.$id : undefined
+            })
+                if (dbPost) {
+                  navigator(`/post/${dbPost.$id}`)
+                }  
+        } else{
+
+            const file = await appwriteService.uploadFile(data.image[0]);
+            if (file) {
+                const fileId = file.$id
+                data.featuredImage = fileId
+                const dbPost = await appwriteService.
+                createPost({
+                    ...data,
+                    userId: useRouteLoaderData.$id,
+                }) 
+                if (dbPost) {
+                    navigat(`/post/${dbPost.$id}`)
+                }
+            }
+        }
+    }
+
+    const  slugTransform = useCallback((value) => {
+        if (value && typeof value === "string")
+            return value
+            .trim()
+            .toLowerCase()
+            .replace(/^[a-zA-Z\d\s]+/g, '-')
+            .replace(/\s/g, '-')
+        return ''
+    },[])
   return (
     <div>
       
