67f15c545dc2c74ae4769c659e201ba12f73730c
Tejasvini <tejasvini.patel1311@gmail.com>
2024-04-01 17:53:04 +0530

    main file import

--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -5,16 +5,74 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+// import {AuthLayout,Login} from './index.js'
+import AddPost from './pages/Post.jsx'
+import Signup from './pages/Signup.jsx'
+import EditPost from "./pages/EditPost"
+import AllPosts from "./pages/AllPosts";
+
 
 
 const router = createBrowserRouter([
   {
-    path:'/',
+    path: "/",
     element: <App/>,
-    children:[
-      
-    ]
-  }
+    children: [
+        {
+            path: "/",
+            element: <Home/>,
+        },
+        {
+            path: "/login",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Login/>
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Signup/>
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/all-posts",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AllPosts/>
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/add-post",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AddPost/>
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/edit-post/:slug",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <EditPost/>
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/post/:slug",
+            element: <Post/>,
+        },
+    ],
+},
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
