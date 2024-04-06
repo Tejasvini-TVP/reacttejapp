4ade8ffdb90aec7f208afbf6dba362a30ab4da32
Tejasvini <tejasvini.patel1311@gmail.com>
2024-04-06 16:02:45 +0530

    Navbar

--- visitor app/src/App.jsx
+++ visitor app/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar/Navbar"
+import Sidebar from "./components/Sidebar/Sidebar"
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-     welcome to Tejasvini house
-
+   <Navbar/>
+   <Sidebar/>
     </>
   )
 }
---
+++ visitor app/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Drawer from '@mui/joy/Drawer';
+import DialogTitle from '@mui/joy/DialogTitle';
+import ModalClose from '@mui/joy/ModalClose';
+
+
+
+export default function Sidebar() {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Button variant="outlined" color="neutral" onClick={() => setOpen(true)}>
+        Open drawer
+      </Button>
+      <Drawer open={open} onClose={() => setOpen(false)}>
+        <ModalClose />
+        <DialogTitle>Visitor</DialogTitle>
+      </Drawer>
+    </Box>
+  );
+}
